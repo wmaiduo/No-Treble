b68f3cc1f6f1405a107af8639577887eb4b77c30
tommy <trantommy@protonmail.ch>
2021-10-21 17:46:25 -0700

    initial commit

---
+++ src/components/Application.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Application() {
+  return <h2>Hello from React!</h2>
+}
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from "./components/Application";
+
+function render() {
+  const root = document.getElementById("root");
+  ReactDOM.render(<App />, root);
+}
+render();

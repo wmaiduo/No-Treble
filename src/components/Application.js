52d6f6f657d8170607abedf5c9deee7716645b20
Anshu Pasrija <anshu.pasrija@gmail.com>
2021-10-24 18:19:35 -0700

    add sidebar

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import './Application.css'
+
 
 export default function Application() {
-  return <h2>Hello from React!</h2>
+  return (
+    <div className = "layout">
+    <div class="sidebar">
+    <h2 class="heading">No Treble </h2>
+    <a class="active" href="#home"> <span class="fa fa-home" aria-hidden="true"></span>Home</a>
+    <a href="#"><i class="fa fa-heart"></i>Favourites</a>
+    <a href="#"><i class="fa fa-user"></i>Artists</a>
+    <a href="#"><i class="fa fa-camera"></i>Album </a>   
+  
+    
+    </div>
+    <div className="searchbar">
+    <input  className= "input" placeholder="Search.." />
+    <i class="fas fa-search"></i>
+  
+
+  
+    </div>
+
+    </div>
+  )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import "./index.css";
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from "./components/Application";
+import 'font-awesome/css/font-awesome.min.css'; 
 
 function render() {
   const root = document.getElementById("root");

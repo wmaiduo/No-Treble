5938f0aacf1bdea016045a8a49411a12206b56eb
mai Duo Wu <mwu296@uwo.ca>
2021-10-26 02:33:12 -0700

    fix the bug where music restarts when new songs are added to the current playlist and adjust the default volume

--- src/client/components/MusicPlayer.jsx
+++ src/client/components/MusicPlayer.jsx
@@ -6,21 +6,22 @@ import 'react-jinke-music-player/assets/index.css';
 import { dummyData1, dummyData2 } from './MusicPlayerDummyData/dummyAudioListPaths';
 
 const MusicPlayer = () => {
-    const [audioLists, setAudioLists] = useState(dummyData2);
+    const [audioLists, setAudioLists] = useState(dummyData1);
 
     const options = {
-        audioLists: audioLists,
         theme: "dark",
         mode: "full",
         remove: true,
         autoPlay: true,
         quietUpdate: true,
         clearPriorAudioLists: true,
+        audioLists: audioLists,
+        defaultVolume: 0.5,
     }
 
     useEffect(() => {
         setTimeout(() => {
-          setAudioLists([...dummyData2, ...dummyData1])
+          setAudioLists([...dummyData1, ...dummyData2]);
         }, 5000)
       }, [setAudioLists])
 

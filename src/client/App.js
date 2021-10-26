9a8190ec152f15ec48df8add517525d7ae291fec
tommy <trantommy@protonmail.ch>
2021-10-25 18:06:40 -0700

    import old code into new application

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './react.png';
+import Sidebar from './components/Sidebar.jsx';
+import Searchbar from './components/Searchbar.jsx';
+import Cards from './components/Cards.jsx';
+import Controller from './components/Controller.jsx';
 
 export default class App extends Component {
-  state = { username: null };
-
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
-
   render() {
-    const { username } = this.state;
     return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+      <div className = "layout">
+      <Sidebar/>
+      <div className="container">
+      <Searchbar/>
+      <Cards/>
+      <Controller/>
+      </div>
       </div>
     );
   }

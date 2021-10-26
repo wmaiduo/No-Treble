310c5d8ae5b8d63353598fcc99023ee6a4e6ec16
mai Duo Wu <mwu296@uwo.ca>
2021-10-26 03:32:55 -0700

    add options hook to change so clearPrioAudioLists changes according to whether we wish the current song to be played when the current playlist is altered

--- src/client/components/MusicPlayer.jsx
+++ src/client/components/MusicPlayer.jsx
@@ -1,33 +1,52 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-import ReactJkMusicPlayer from 'react-jinke-music-player';
-import 'react-jinke-music-player/assets/index.css';
+import ReactJkMusicPlayer from "react-jinke-music-player";
+import "react-jinke-music-player/assets/index.css";
 
-import { dummyData1, dummyData2 } from './MusicPlayerDummyData/dummyAudioListPaths';
+import {
+  dummyData1,
+  dummyData2,
+  dummyData3, 
+} from "./MusicPlayerDummyData/dummyAudioListPaths";
 
 const MusicPlayer = () => {
-    const [audioLists, setAudioLists] = useState(dummyData1);
+  const [audioLists, setAudioLists] = useState(dummyData1);
+  const [options, setOptions] = useState({
+    theme: "dark",
+    mode: "full",
+    remove: true,
+    autoPlay: true,
+    quietUpdate: true,
+    clearPriorAudioLists: true,
+    audioLists: audioLists,
+    defaultVolume: 0.5,
+  });
 
-    const options = {
-        theme: "dark",
-        mode: "full",
-        remove: true,
-        autoPlay: true,
-        quietUpdate: true,
+  //the following example shows when we try to add music to the existing playlist
+  useEffect(() => {
+    setTimeout(() => {
+      setOptions({
+        ...options,
+        audioLists: [...audioLists, ...dummyData2],
+        clearPriorAudioLists: false,
+      });
+      setAudioLists([...audioLists, ...dummyData2]);
+    }, 3000);
+  }, [setOptions]);
+
+  //the following example show when we try to replace the current playlist with a new playlist
+  useEffect(() => {
+    setTimeout(() => {
+      setOptions({
+        ...options,
+        audioLists: [...dummyData3],
         clearPriorAudioLists: true,
-        audioLists: audioLists,
-        defaultVolume: 0.5,
-    }
+      });
+      setAudioLists([...dummyData3]);
+    }, 6000);
+  }, [setOptions]);
 
-    useEffect(() => {
-        setTimeout(() => {
-          setAudioLists([...dummyData1, ...dummyData2]);
-        }, 5000)
-      }, [setAudioLists])
-
-    return (
-        <ReactJkMusicPlayer {...options} />
-    )
-}
+  return <ReactJkMusicPlayer {...options} />;
+};
 
 export default MusicPlayer;
--- src/client/components/MusicPlayerDummyData/dummyAudioListPaths.js
+++ src/client/components/MusicPlayerDummyData/dummyAudioListPaths.js
@@ -1,5 +1,62 @@
 const dummyData1 = [
   {
+    name: "Fly Me to the Moon",
+    singer: "The Macarons Project",
+    cover:
+      "https://e-cdns-images.dzcdn.net/images/cover/f0bc5ad6c844e59f7e832b1b4085ec4b/500x500-000000-80-0-0.jpg",
+    musicSrc: () => {
+      return Promise.resolve(
+        "https://cdns-preview-c.dzcdn.net/stream/c-c2a6681b22273c0b5266a0c969574142-6.mp3"
+      );
+    },
+  },
+  {
+    name: "C'est Si Bon",
+    singer: "Lisa Ono",
+    cover:
+      "https://e-cdns-images.dzcdn.net/images/artist/3acac82be11a17763d8c434a0a8b8518/500x500-000000-80-0-0.jpg",
+    musicSrc:
+      "https://cdns-preview-7.dzcdn.net/stream/c-7d7bd0dcb0c3dcc790c46aea855aba38-5.mp3",
+  },
+  {
+    name: "Who Can I Turn To?",
+    singer: "Katharine McPhee",
+    cover:
+      "https://e-cdns-images.dzcdn.net/images/artist/05fbe36e427817f0b5b4e60565a81298/500x500-000000-80-0-0.jpg",
+    musicSrc:
+      "https://cdns-preview-5.dzcdn.net/stream/c-508490507ed9702a317939cc8bf18bda-8.mp3",
+  },
+];
+
+const dummyData2 = [
+  {
+    name: `Solar Sect of Mystic Wisdom ~ Nuclear Fusion (From "Touhou 11")`,
+    singer: "FalKKonE",
+    cover:
+      "https://e-cdns-images.dzcdn.net/images/cover/5c6dffe7d0c6f655bd259373642bec64/500x500-000000-80-0-0.jpg",
+    musicSrc:
+      "https://cdns-preview-e.dzcdn.net/stream/c-e2b854a79f9936186f758d023d53783a-4.mp3",
+  },
+  {
+    name: "Lucid Traveler（feat. NAGISA）",
+    singer: "A-One",
+    cover:
+      "https://e-cdns-images.dzcdn.net/images/cover/aca1f9b1ba24ab1ac62ac8fc7588e116/500x500-000000-80-0-0.jpg",
+    musicSrc:
+      "https://cdns-preview-1.dzcdn.net/stream/c-152e9c4d20ebb16df82bebfd820935f0-3.mp3",
+  },
+  {
+    name: "Loreilei's Melody",
+    singer: "Takahito Rute Koshida",
+    cover:
+      "https://e-cdns-images.dzcdn.net/images/cover/8043299a9639876e12e9aa65f52278e6/500x500-000000-80-0-0.jpg",
+    musicSrc:
+      "https://cdns-preview-8.dzcdn.net/stream/c-87726436d6fb70416ac42df1088760a9-3.mp3",
+  },
+];
+
+const dummyData3 = [
+  {
     name: "Bedtime Stories",
     singer: "Jay Chou",
     cover:
@@ -28,34 +85,4 @@ const dummyData1 = [
   },
 ];
 
-const dummyData2 = [
-  {
-    name: "Fly Me to the Moon",
-    singer: "The Macarons Project",
-    cover:
-      "https://e-cdns-images.dzcdn.net/images/cover/f0bc5ad6c844e59f7e832b1b4085ec4b/500x500-000000-80-0-0.jpg",
-    musicSrc: () => {
-      return Promise.resolve(
-        "https://cdns-preview-c.dzcdn.net/stream/c-c2a6681b22273c0b5266a0c969574142-6.mp3"
-      );
-    },
-  },
-  {
-    name: "C'est Si Bon",
-    singer: "Lisa Ono",
-    cover:
-      "https://e-cdns-images.dzcdn.net/images/artist/3acac82be11a17763d8c434a0a8b8518/500x500-000000-80-0-0.jpg",
-    musicSrc:
-      "https://cdns-preview-7.dzcdn.net/stream/c-7d7bd0dcb0c3dcc790c46aea855aba38-5.mp3",
-  },
-  {
-    name: `Solar Sect of Mystic Wisdom ~ Nuclear Fusion (From "Touhou 11")`,
-    singer: "FalKKonE",
-    cover:
-      "https://e-cdns-images.dzcdn.net/images/cover/5c6dffe7d0c6f655bd259373642bec64/500x500-000000-80-0-0.jpg",
-    musicSrc:
-      "https://cdns-preview-e.dzcdn.net/stream/c-e2b854a79f9936186f758d023d53783a-4.mp3",
-  },
-];
-
-export { dummyData1, dummyData2 };
+export { dummyData1, dummyData2, dummyData3 };

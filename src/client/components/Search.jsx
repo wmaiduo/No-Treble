e5aa05c1ab4c9ec9b3d9c5a31b855d0391e98236
mai Duo Wu <mwu296@uwo.ca>
2021-10-26 22:40:22 -0700

    add live search function in Search jsx, the JSON would be return 0.75 seconds after nothing is entered inside the search bar

--- src/client/components/Search.jsx
+++ src/client/components/Search.jsx
@@ -1,22 +1,42 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 const Search = () => {
-  const [search, setSearch] = useState({value: "",});
-  const [musicData, setMusicData] = useState({})
+  const [search, setSearch] = useState({ value: "" });
+  const [musicData, setMusicData] = useState(null);
 
   const onChangeHandler = (e) => {
-    setSearch({value: e.target.value});
+    setSearch({ value: e.target.value });
   };
 
   useEffect(() => {
-    Promise.resolve(
-        axios.get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${search}`)
-    ).then(value => {
-        console.log(value);
-        setMusicData({value});
-    })
+    // Promise.resolve(
+    //     axios.get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${search}`)
+    // ).then(value => {
+    //     console.log(value);
+    //     setMusicData({value});
+    // })
+    const timeOutID = setTimeout(() => {
+      Promise.resolve(
+
+        //https://cors-anywhere.herokuapp.com/ is added to avoid CORS error
+        axios.get(
+          `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${search.value}`
+        )
+      ).then((res) => {
+        if(res.data.error) {
+          setMusicData(null);
+        } else {
+        setMusicData(res.data.data);
+        }
+      });
+    }, 750);
+    return () => clearTimeout(timeOutID);
   }, [search]);
+
+  useEffect(() => {
+    console.log("music data shown: ", musicData)
+  }, [musicData])
 
   return (
     <div className="searchbar">

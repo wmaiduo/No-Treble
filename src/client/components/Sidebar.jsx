46e70dfdca958b937d7836f146f3355227299445
mai Duo Wu <mwu296@uwo.ca>
2021-10-28 02:51:11 -0700

    add hooks to sidebar so active, hovering states can be determined and used for styles in styled components

--- src/client/components/Sidebar.jsx
+++ src/client/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import styled from "styled-components";
 
@@ -14,33 +14,106 @@ const SidebarContainer = styled.div`
 `;
 
 const Heading = styled.h1`
-  color: #04aa6d;
+  color: #04AA6d;
   display: flex;
   margin: auto;
-  margin-top: 4rem;
-  margin-bottom: 5rem;
+  margin-top: 3rem;
+  margin-bottom: 3rem;
   margin-left: 2rem;
-  font-family: 'Monoton', cursive, Times;
+  font-family: "Monoton", cursive;
   font-size: 3rem;
 `;
 
+const Link = styled.a`
+  font-family: 'Space Mono', monospace;
+  display: block;
+  color: white;
+  padding: 16px;
+  text-decoration: none;
+  background: ${(props) =>
+    props.linkState === "active"
+      ? "#04AA6D"
+      : props.linkState === "hover"
+      ? "#555"
+      : "transparent"};
+`;
+
+// .sidebar a {
+//   display: block;
+//   color: black;
+//   padding: 16px;
+//   text-decoration: none;
+//   color:white;
+// }
+
+// /* Active/current link */
+// .sidebar a.active {
+//   background-color: #04AA6D;
+//   color: white;
+// }
+
+// /* Links on mouse-over */
+// .sidebar a:hover:not(.active) {
+//   background-color: #555;
+//   color: white;
+// }
+// .fa{
+//   padding:8px;
+// }
+
 function Sidebar() {
+  const [active, setActive] = useState(null);
+  const [hover, setHover] = useState(null);
+
   return (
-    <SidebarContainer className="sidebar">
-      <Heading>NO TREBLE</Heading>
-      <a className="active" href="#home">
-        <span className="fa fa-home" aria-hidden="true"></span>Home
-      </a>
-      <a href="#">
-        <i className="fa fa-heart"></i>Favourites
-      </a>
-      <a href="#">
-        <i className="fa fa-user"></i>Artists
-      </a>
-      <a href="#">
-        <i className="fa fa-camera"></i>Album{" "}
-      </a>
-    </SidebarContainer>
+    <React.Fragment>
+      <SidebarContainer>
+        <Heading>NOTREBLE</Heading>
+        <Link
+          onClick={() => setActive("home")}
+          onMouseEnter={() => setHover("home")}
+          onMouseLeave={() => setHover(null)}
+          linkState={
+            active === "home" ? "active" : hover === "home" ? "hover" : null
+          }
+        >
+          <span className="fa fa-home" aria-hidden="true"></span>Home
+        </Link>
+        <Link
+          href="#"
+          onClick={() => setActive("favorites")}
+          onMouseEnter={() => setHover("favorites")}
+          onMouseLeave={() => setHover(null)}
+          linkState={
+            active === "favorites" ? "active" : hover === "favorites" ? "hover" : null
+          }
+        >
+          <i className="fa fa-heart"></i>Favourites
+        </Link>
+        <Link
+          href="#"
+          onClick={() => setActive("artists")}
+          onMouseEnter={() => setHover("artists")}
+          onMouseLeave={() => setHover(null)}
+          linkState={
+            active === "artists" ? "active" : hover === "artists" ? "hover" : null
+          }
+        >
+          <i className="fa fa-user"></i>Artists
+        </Link>
+        <Link
+          href="#"
+          onClick={() => setActive("album")}
+          onMouseEnter={() => setHover("album")}
+          onMouseLeave={() => setHover(null)}
+          linkState={
+            active === "album" ? "active" : hover === "album" ? "hover" : null
+          }
+        >
+          <i className="fa fa-camera"></i>Album{" "}
+        </Link>
+      </SidebarContainer>
+    </React.Fragment>
   );
 }
 

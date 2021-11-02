b990f35a644c469ea1da6bc0a9bd4f8126f28305
tommy <trantommy@protonmail.ch>
2021-11-01 18:04:18 -0700

    bug fix - move server back to /src

--- electron/src/main.js
+++ electron/src/main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const server = require('./server/server.js');
+const server = require('./server.js');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
